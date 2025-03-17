65ce61eaf4adb1792e14744527aa936c3acc22c1
Denis Bondarenko <danbond85@mail.ru>
2025-03-17 22:42:29 +0300

    Login: Notifications implemented

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,9 +4,11 @@ import { Card, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
 import AuthForm from "@/app/components/AuthForm";
 import { useAuthStore } from "@/app/store/auth";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
 
 export default function Page() {
   const router = useRouter();
+  // const { enqueueSnackbar } = useSnackbar();
 
   const handleSubmit = async (data: AuthData) => {
     const loggedIn = await useAuthStore.getState().login(data);
@@ -14,15 +16,15 @@ export default function Page() {
     if (loggedIn) {
       router.push("/backoffice/users");
     } else {
-      /**
-       * @todo add notification
-       */
-      alert("Неправильный логин или пароль");
+      enqueueSnackbar("Неверный логин или пароль", { variant: "error" });
     }
   };
 
   return (
     <Card sx={{ p: 5, textAlign: "center" }}>
+      <SnackbarProvider
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      />
       <Typography variant="h5">Авторизация</Typography>
       <AuthForm onSubmit={handleSubmit} />
     </Card>
